85582086b5ba55ba915dcabbc9cf2fcbfe30d767
Mike <mishanosov@yandex.ru>
2024-04-09 21:29:21 +0300

    Update hikvision.py

--- unifi/cams/hikvision.py
+++ unifi/cams/hikvision.py
@@ -120,7 +120,7 @@ class HikvisionCam(UnifiCamBase):
 
     async def run(self) -> None:
         self.ptz_supported = await self.check_ptz_support(self.channel)
-        # return
+        return
 
         while True:
             self.logger.info("Connecting to motion events API")
